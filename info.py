e69d32376e607b00be28703ab8b0ab860c20effa
Tech2705 <160301845+Tech2705@users.noreply.github.com>
2024-10-08 19:02:58 +0530

    info.py

--- info.py
+++ info.py
@@ -27,6 +27,6 @@ MONGO_DB = str(
 DEF_CAP = str(
     getenv(
         "DEF_CAP",
-        "<b><a href='telegram.me/bisal_files'>{file_name} Telegram : @Bisal_Files\n\nForward the file before Downloading.</a></b>",
+        "<b><a href='telegram.me/A2ZMovieRequesst'>{file_name} Telegram : @A2ZMovieRequesst\n\nForward the file before Downloading.</a></b>",
     )
 )
